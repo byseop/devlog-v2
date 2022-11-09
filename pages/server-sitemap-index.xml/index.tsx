f4c7f9b99338376b09218668dd939e64f9f50f2c
Youngseop Bae <byseop@gmail.com>
2022-11-09 15:32:31 +0900

    feat: sitemap.xml, robot.txt 추가

---
+++ pages/server-sitemap-index.xml/index.tsx
@@ -0,0 +1,21 @@
+import { getServerSideSitemapIndex } from 'next-sitemap';
+import { postApis } from '../../core/apis/posts';
+
+import type { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const res = await postApis.getPosts();
+    return getServerSideSitemapIndex(
+      context,
+      res.data.map((page) => `https://byseop.com/posts/@${page.id}`)
+    );
+  } catch (e) {
+    console.error(e);
+    return {
+      notFound: true
+    };
+  }
+};
+
+export default function SitemapIndex() {}
